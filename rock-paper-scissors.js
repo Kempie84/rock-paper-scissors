881810c4cfd80bfe443e2314131aa700e416c520
Kempie84 <67120277+Kempie84@users.noreply.github.com>
2025-11-11 15:23:18 +0100

    add comments to describe all subproblems

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,31 @@
 console.log("Hello World")
+
+//Declare computer choice variable of type string default value ""
+
+//Declare user choice variable of type string default value ""
+
+/*Function getComputerChoice() will generate computer choice of rock, paper or scissors 
+and store it in computer choice variable, using math.random to generate random choice */
+
+/*Function getHumanChoice() will get the choice of rock, paper or scissors using a prompt:
+"Choose your weapon (only rock, paper or scissors)". Store choice in user choice variable */
+
+// Declare humanScore variable type int default value 0
+
+// Declare computerScore variable type int default value 0
+
+/* Create function playRound that compares the choices of Human and Computer,
+it takes two parameters as arguments(humanChoice and computerChoice), 
+stores the choices in new constants.
+
+compare choices and declare a winner using console.log
+
+Increment winners score by 1 */
+
+/* Create function playGame()
+
+It contains function playRound() and the score variables
+
+It plays five rounds using a for loop.
+
+
